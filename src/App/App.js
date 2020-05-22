14e1e7f161bcbf76e74ffa19428e9725b8a69e8f
Nicky Pochinkov <pochinkn@tcd.ie>
2020-05-22 03:37:16 +0100

    Added Color Circle Option

--- src/App/App.js
+++ src/App/App.js
@@ -30,10 +30,14 @@ const App = () => {
   const [numberOfBars, setNumberOfBars] = useState(DEFAULT_NUMBER_OF_BARS)
   const [targets, setTargets] = useState([3,7])
   const [barHeights, setBarHeights] = useState([])
+  
   const [delay, setDelay] = useState()
   const [isRunning, setRunning] = useState(false)
   const [sortAlgorithm, setSortAlgorithm] = useState("")
-
+  
+  const toggleCircleDisplay = (event) => setCircleDisplay(event.target.checked);
+  const [isCircleDisplay, setCircleDisplay] = useState(true)
+  
   const isTarget = (index) => (targets.includes(index))
   const isInOrder = (arr, i, j) => (arr[i] <= arr[j])
   const areTargetsInOrder = (i,j) => isInOrder(barHeights, targets[0], targets[1]) 
@@ -102,15 +106,26 @@ const App = () => {
             Generate New Bars
           </button>
         </div>
-        
-        <div class="sortAlgorithmButtons">
+
+        <label className="menuLabel" htmlFor="circleDisplay">
+          Bars 
+          <Switch
+            checked={isCircleDisplay}
+            onChange={toggleCircleDisplay}
+            name="circleDisplay"
+            inputProps={{ 'aria-label': 'secondary checkbox' }}
+          />
+          Circle
+        </label>
+
+        <div className="sortAlgorithmButtons">
         {menuItems.map((text, index) => (
           <button 
             className="menuButton" 
             to="#" 
             key={text} 
             value={text}
-            onClick={chooseAlgorithm}
+            onClick={handleSortChoice}
             style={!text.localeCompare(sortAlgorithm) ? {color: "purple"} : {}}>
               {text}
           </button>
@@ -125,16 +140,37 @@ const App = () => {
         </button>
 
       </Menu>
-      <DisplayArea style={{gridTemplateColumns: `repeat(${numberOfBars}, 1fr)`}}>
-          {/* List the bars for the bar chart */}
-          {barHeights.map((h, index)=>(
+      {/* List the bars for the bar chart */}
+      { isCircleDisplay ? 
+        <DisplayArea style={{display: "flex", flexDirection:"column", alignItems: "center", justifyContent: "flex-end"}}>
+          <svg id="mid-circle" viewBox="0 0 200 200" style={{width:"80vmin", height:"80vmin"}}>
+            
+            {barHeights.map((h, index) => (
+              <polygon points={`100,100  100,0  ${100*(1 + sin( 1/numberOfBars ))},${ 100*(1 - cos( 1/numberOfBars ))}`}
+                       key={index} 
+                       fill={`hsl(${h*360 }, 100%, 70%)`}
+                       stroke={(isTarget(index)) ? (areTargetsInOrder()?"green":"red") : ""}
+                       style={{
+                        transformOrigin: "50% 50%",
+                        transform: `rotate(${ index*360/(numberOfBars) }deg)`,
+                       }}>
+              </polygon>
+            ))
+            
+            }
+            </svg>
+        </DisplayArea> 
+            : 
+        <DisplayArea style={{gridTemplateColumns: `repeat(${numberOfBars}, 1fr)`}}>
+            {barHeights.map((h, index)=>(
             <Bar height={h} 
                  key={index} 
                  selected={ (isTarget(index)) ? (areTargetsInOrder()?"good":"bad") : false }>
             </Bar>
           ))}
+        </DisplayArea>
+        }
 
-      </DisplayArea>
     </div>
   )
 }
